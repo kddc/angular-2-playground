27fce34f53331619a3951e01e81909e379481df2
KddC <Jutiemann@gmail.com>
2016-08-16 16:10:48 +0200

    tutorial chapter 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { MdToolbarModule } from '@angular2-material/toolbar/toolbar';
+import { MdButtonModule } from '@angular2-material/button/button';
 import { MdCardModule } from '@angular2-material/card/card';
 // import {MdButtonToggleModule} from '@angular2-material/button-toggle/button-toggle';
-// import {MdButtonModule} from '@angular2-material/button/button';
 // import {MdCheckboxModule} from '@angular2-material/checkbox/checkbox';
 // import {MdRadioModule} from '@angular2-material/radio/radio';
 // import {MdSlideToggleModule} from '@angular2-material/slide-toggle/slide-toggle';
 // import {MdSliderModule} from '@angular2-material/slider/slider';
-// import {MdSidenavModule} from '@angular2-material/sidenav/sidenav';
-// import {MdListModule} from '@angular2-material/list/list';
+import { MdSidenavModule } from '@angular2-material/sidenav/sidenav';
+import { MdListModule } from '@angular2-material/list/list';
 // import {MdGridListModule} from '@angular2-material/grid-list/grid-list';
-// import {MdIconModule} from '@angular2-material/icon/icon';
+import { MdIconModule } from '@angular2-material/icon/icon';
 // import {MdProgressCircleModule} from '@angular2-material/progress-circle/progress-circle';
 // import {MdProgressBarModule} from '@angular2-material/progress-bar/progress-bar';
 // import {MdInputModule} from '@angular2-material/input/input';
 // import {MdTabsModule} from '@angular2-material/tabs/tabs';
 // import {MdTooltipModule} from '@angular2-material/tooltip/tooltip';
-// import {MdRippleModule} from '@angular2-material/core/ripple/ripple';
+// import { MdRippleModule } from '@angular2-material/core/ripple/ripple';
 // import {PortalModule} from '@angular2-material/core/portal/portal-directives';
 // import {OverlayModule} from '@angular2-material/core/overlay/overlay-directives';
 // import {MdMenuModule} from '@angular2-material/menu/menu';
@@ -32,8 +32,12 @@ import { TestComponent } from './test/test.component'
   imports: [
     BrowserModule,
     FormsModule,
+    MdSidenavModule,
     MdToolbarModule,
-    MdCardModule
+    MdCardModule,
+    MdListModule,
+    MdButtonModule,
+    MdIconModule
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
