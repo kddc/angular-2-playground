925885954bc653e1b1f09b00d257de7a5d92607e
KddC <Jutiemann@gmail.com>
2016-08-17 16:28:51 +0200

    rmeove unused code

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,32 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero, HeroService, HeroListComponent, HeroDetailComponent } from '../shared/heroes';
+import { Hero, HeroService } from '../shared/heroes';
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
-  styles: [ require('./dashboard.component.scss').toString() ],
-  directives: [ HeroListComponent ]
+  styles: [ require('./dashboard.component.scss').toString() ]
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
   heroService: HeroService;
 
   constructor(heroService: HeroService) {
     this.heroService = heroService;
   }
 
-  onSelectHero(hero: Hero) {
-    this.selectedHero = hero;
-  }
-
-  // selectHero(hero: Hero) {
-  //   this.selectedHero = hero;
-  // }
-
   ngOnInit() {
-    // this.onSelect = this.selectHero.bind(this);
     this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 }
